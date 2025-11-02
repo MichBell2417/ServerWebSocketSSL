708d0bdeca4aab0202ccca2e7994171c8daef3dc
MichBell2417 <belmichi07@gmail.com>
2025-11-03 00:11:38 +0100

    inizio server e primi test (fallimentari)

---
+++ server_base.py
@@ -0,0 +1,21 @@
+import asyncio
+import websockets
+import ssl
+import pathlib
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+localhost_pem = pathlib.Path(__file__).with_name("localhost.pem")
+ssl_context.load_cert_chain(certfile="cert.pem", keyfile="key.pem")
+
+async def manager(websocket):
+    print("connected")
+    async for message in websocket:
+        print(message)
+    print("disconnected")
+
+async def start():
+    async with websockets.serve(manager, "", 8080, ssl=ssl_context):
+        await asyncio.Future()  # run forever
+
+if __name__ == "__main__":
+    asyncio.run(start())
